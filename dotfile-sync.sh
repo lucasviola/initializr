a32f5ae701cd16d0c5ffff3e487d37a79f687a16
lucasviola <lucasmatzenbacher@gmail.com>
2016-01-06 19:23:29 -0200

    Backs up old dotfiles

--- dotfile-sync.sh
+++ dotfile-sync.sh
@@ -6,3 +6,14 @@ read link
 
 git clone $link
 
+# Synchronize symbolic links
+DIR=$PWD/dotfiles
+OLDDIR=$PWD/DOTFILES_BAK
+FILES="bashrc vimrc vim zshrc oh-my-zsh"
+
+echo "Backing up old dotfiles..."
+mkdir -p $OLDDIR
+
+for FILE in $FILES; do
+	cp ~/.$FILE ~/$OLDDIR/.$FILE
+
