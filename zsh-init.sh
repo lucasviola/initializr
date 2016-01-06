3bdf9c162f86d934c2e024d1c7c790987767ae01
lucasviola <lucasmatzenbacher@gmail.com>
2016-01-06 16:28:14 -0200

    Installs zsh

--- zsh-init.sh
+++ zsh-init.sh
@@ -2,12 +2,15 @@
 
 echo 'Make sure you are connected to the internet!'
 
-# Installing ZSH from source
-#wget -nv http://downloads.sourceforge.net/project/zsh/zsh/5.2/zsh-5.2.tar.gz?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fzsh%2Ffiles%2Fzsh%2F5.2%2F&ts=1452102213&use_mirror=ufpr
-#tar -zxf zsh-5.2.tar.gz\?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fzsh%2Ffiles%2Fzsh%2F5.2%2F\&ts=1452102213\&use_mirror=ufpr
-#rm zsh-5.2.tar.gz\?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fzsh%2Ffiles%2Fzsh%2F5.2%2F\&ts=1452102213\&use_mirror=ufpr
-
+# Downloading and unzipping zsh
 link=http://downloads.sourceforge.net/project/zsh/zsh/5.2/zsh-5.2.tar.gz\?r\=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fzsh%2Ffiles%2Fzsh%2F5.2%2F\&ts\=1452102213\&use_mirror\=ufpr
 
 wget -qO- $link | tar xvz -C ~/Workspace/initializr
 
+# Installing zsh
+cd zsh-5.2
+./configure
+make
+sudo make install
+
+echo 'ZSH installed with success!'
