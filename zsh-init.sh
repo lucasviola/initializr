8c2378a39631855116188947c17008f7048ef6e6
lucasviola <lucasmatzenbacher@gmail.com>
2016-01-06 18:42:54 +0000

    Fixes path

--- zsh-init.sh
+++ zsh-init.sh
@@ -5,7 +5,7 @@ echo 'Make sure you are connected to the internet!'
 # Downloading and unzipping zsh
 link=http://downloads.sourceforge.net/project/zsh/zsh/5.2/zsh-5.2.tar.gz\?r\=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fzsh%2Ffiles%2Fzsh%2F5.2%2F\&ts\=1452102213\&use_mirror\=ufpr
 
-wget -qO- $link | tar xvz -C ~/Workspace/initializr
+wget -qO- $link | tar xvz -C $PWD
 
 # Installing zsh
 cd zsh-5.2
