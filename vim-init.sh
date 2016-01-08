11bce750736c158f97d9b32aa3e14d3168fbcc32
lucasviola <lucasmatzenbacher@gmail.com>
2016-01-08 17:00:30 +0000

    Fixing if else statement

--- vim-init.sh
+++ vim-init.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 # Downloading pathogen
-#mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-#wget https://tpo.pe/pathogen.vim
-#mv pathogen.vim ~/.vim/autoload/pathogen.vim
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+wget https://tpo.pe/pathogen.vim
+mv pathogen.vim ~/.vim/autoload/pathogen.vim
 
-if [ -f "~/.vimrc" ]; then
+# Not Working
+if [ -f "~/.vimrc" && !$(cat ~/.vimrc | grep pathogen) ]; then
 	echo 'execute pathogen#infect()' >> ~/.vimrc
 	echo 'syntax on' >> ~/.vimrc
 	echo 'filetype plugin indent on' >> ~/.vimrc
