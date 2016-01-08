005f023eb89bb46b69d5e70c1aa7c17f6957e67d
lucasviola <lucasmatzenbacher@gmail.com>
2016-01-08 16:52:05 +0000

    Fixes if statement

--- vim-init.sh
+++ vim-init.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 # Downloading pathogen
-mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-wget https://tpo.pe/pathogen.vim
-mv pathogen.vim ~/.vim/autoload/pathogen.vim it clone https://github.com/tpope/vim-pathogen.git
+#mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+#wget https://tpo.pe/pathogen.vim
+#mv pathogen.vim ~/.vim/autoload/pathogen.vim
 
-if [ -f ~/.vimrc ]; then
-	echo 'execute pathogen#infect()' > .vimrc
-	echo 'syntax on' >> .vimrc
-	echo 'filetype plugin indent on' >> .vimrc
-fi'
+if [ -f "~/.vimrc" ]; then
+	echo 'execute pathogen#infect()' >> ~/.vimrc
+	echo 'syntax on' >> ~/.vimrc
+	echo 'filetype plugin indent on' >> ~/.vimrc
+fi
 
