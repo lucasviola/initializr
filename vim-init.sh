2a52059a0a8102efea2bbcf7347a220db4cf7a4a
lucasviola <lucasmatzenbacher@gmail.com>
2016-01-09 18:50:53 +0000

    Fixes if statement

--- vim-init.sh
+++ vim-init.sh
@@ -5,8 +5,7 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 wget https://tpo.pe/pathogen.vim
 mv pathogen.vim ~/.vim/autoload/pathogen.vim
 
-# Not Working
-if [ -f "~/.vimrc" && !$(cat ~/.vimrc | grep pathogen) ]; then
+if [ -f ~/.vimrc ]; then
 	echo 'execute pathogen#infect()' >> ~/.vimrc
 	echo 'syntax on' >> ~/.vimrc
 	echo 'filetype plugin indent on' >> ~/.vimrc
